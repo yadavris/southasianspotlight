96648ff66d758af6fab6ef32701333a078e33a78
yadavris <144559173+yadavris@users.noreply.github.com>
2025-08-07 22:08:32 -0500

    Resolve router conflict by using only wouter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/southasianspotlight">
-    <App />
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
